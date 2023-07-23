2b0f15f4a08907ac80af739f6b41f21f95fdbe28
meluiz <me.luizfelipe@gmail.com>
2023-07-23 04:50:40 -0300

    refactor(camel-case.test.ts): update test description to be more descriptive and accurate

--- tests/string/cases/camel-case.test.ts
+++ tests/string/cases/camel-case.test.ts
@@ -3,7 +3,7 @@ import { describe, expect, it } from 'vitest'
 import { Str } from '../../../src'
 
 describe('Str.camelCase', () => {
-  it('Should be able to set string as camel case', () => {
+  it('Should be able to generate a camelCase string from the given input string.', () => {
     const string = 'hello world'
     const expected = 'helloWorld'
     const camelCase = Str.camelCase(string)
