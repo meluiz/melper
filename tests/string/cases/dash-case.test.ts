e7e7f199f08501c431ca0d3c675fb8d57b0db48f
meluiz <me.luizfelipe@gmail.com>
2023-07-23 05:44:46 -0300

    test(dash-case.test.ts): add test case to generate a dash-case string with capitalize option
    The test case was added to ensure that the `Str.dashCase` function is able to generate a dash-case string with capitalize option from the given input string. The input string "hello world" should be transformed into "Hello-World".

--- tests/string/cases/dash-case.test.ts
+++ tests/string/cases/dash-case.test.ts
@@ -10,4 +10,12 @@ describe('Str.dashCase', () => {
 
     expect(dashCase).toBe(expected)
   })
+
+  it('Should be able to generate a dash-case string with capitalize from the given input string.', () => {
+    const string = 'hello world'
+    const expected = 'Hello-World'
+    const dashCase = Str.dashCase(string, { capitalize: true })
+
+    expect(dashCase).toBe(expected)
+  })
 })
