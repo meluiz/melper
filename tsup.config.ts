73ce089a1188e016602e2894ce1120721ced5d2c
meluiz <me.luizfelipe@gmail.com>
2025-03-30 19:41:03 -0300

    feat: add guard module to build and package configuration
    This commit introduces the `guard` module by adding it to the `tsup.config.ts` entry points and updating the `package.json` exports. The changes ensure the module is included in the build process and can be imported correctly.

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
-  entry: ['./src/index.ts', './src/number.ts', './src/string.ts'],
   dts: true,
   minify: true,
   outDir: 'dist',
   sourcemap: 'inline',
   format: ['cjs', 'esm'],
+  entry: [
+    './src/index.ts',
+    './src/guard.ts',
+    './src/number.ts',
+    './src/string.ts',
+  ],
 })
